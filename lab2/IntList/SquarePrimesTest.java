fc59c87217333c849b255fbe57b725b003799668
XiaoYu <3344609717@qq.com>
2025-01-23 12:20:34 +0800

    finished lab2
    learned how to debug and write tests by self

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -15,6 +15,38 @@ public class SquarePrimesTest {
         IntList lst = IntList.of(14, 15, 16, 17, 18);
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertTrue(changed); // will be true
+    }
+
+    @Test
+    public void testSquarePrimesSimple1() {
+        IntList lst = IntList.of(3, 12, 2, 16, 19, 20);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9 -> 12 -> 4 -> 16 -> 361 -> 20", lst.toString());
+        assertTrue(changed); // will be true
+    }
+
+    @Test
+    public void testSquarePrimesSimple2() {
+        IntList lst = IntList.of(4, 12, 18, 24, 25);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 12 -> 18 -> 24 -> 25", lst.toString());
+        assertFalse(changed); // will be false
+    }
+
+    @Test
+    public void testSquarePrimesSimple3() {
+        IntList lst = IntList.of(4, 6, 9, 10, 16);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 6 -> 9 -> 10 -> 16", lst.toString());
+        assertFalse(changed); // will be false
+    }
+
+    @Test
+    public void testSquarePrimesSimple4() {
+        IntList lst = IntList.of(11, 13, 17, 19, 23);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("121 -> 169 -> 289 -> 361 -> 529", lst.toString());
         assertTrue(changed);
     }
 }
