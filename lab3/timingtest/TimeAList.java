fe825e6083c6bd3f653118b869a3a4d03b00f8a6
XiaoYu <3344609717@qq.com>
2025-02-01 17:52:19 +0800

    lab3

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -29,7 +29,7 @@ public class TimeAList {
         AList<Integer> opCounts = new AList<>();
 
         /** test array. */
-        int[] testSize = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000, 1024000, 2048000, 4096000};
+        int[] testSize = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000};
 
         for (int N: testSize){
             AList<Integer> testList = new AList<>();
