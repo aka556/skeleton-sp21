53c58bba3e1ed25dadd491ebfaddffe878918863
XiaoYu <3344609717@qq.com>
2025-02-09 21:15:25 +0800

    fixed the bug

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -1,5 +1,8 @@
 package flik;
 
+import edu.princeton.cs.algs4.StdRandom;
+import static org.junit.Assert.assertTrue;
+
 /** An Integer tester created by Flik Enterprises.
  * @author Josh Hug
  * */
@@ -8,6 +11,14 @@ public class Flik {
      *  @param b Value 2
      *  @return Whether a and b are the same */
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        /** Integer is Wrapper Class, it's not only compare with numbers,
+         *  but also compare with types.
+         *  for example, Integer a = 128, Integer b = 128,
+         *  they are equal, because they type are not same.
+         *  There can be two methods, one is use int type,
+         *  another is use a.equals(b)
+         */
+        return a.equals(b);
     }
+
 }
