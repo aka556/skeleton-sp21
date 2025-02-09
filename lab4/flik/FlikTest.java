e70f512165edf55ae7d8a4570020b68e2514480f
XiaoYu <3344609717@qq.com>
2025-02-09 21:17:28 +0800

    Write a FlikTest to test

--- lab4/flik/FlikTest.java
+++ lab4/flik/FlikTest.java
@@ -1,2 +1,20 @@
-package flik;public class FlikTest {
+package flik;
+
+import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/** Test the Flik method. */
+public class FlikTest {
+    @Test
+    public void randomizedTest() {
+        for (int i = 0, j = 0; i < 500; i++, j++) {
+            if (!Flik.isSameNumber(i, j)) {
+                throw new AssertionError(
+                        String.format("%d is not same %d", i, j)
+                );
+            }
+        }
+    }
 }
