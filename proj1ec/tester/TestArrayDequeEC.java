d7bd635617962516a86ce54f7bd95f6f66254cc7
xiaoyu <3344609717@qq.com>
2026-07-23 16:53:22 +0800

    fixed some bugs

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -19,120 +19,69 @@ public class TestArrayDequeEC {
 
         int N = 5000;
         for (int i = 0; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 6);
+            int operationNumber = StdRandom.uniform(0, 4);
 
             if (operationNumber == 0) {
-                // addFirst
-                Integer randVal = StdRandom.uniform(0, 100);
-                studentDeque.addFirst(randVal);
-                solutionDeque.addFirst(randVal);
-                assertEquals("addFirst(" + randVal + ")",
-                        solutionDeque.size(), studentDeque.size());
-            } else if (operationNumber == 1) {
-                // addLast
                 Integer randVal = StdRandom.uniform(0, 100);
                 studentDeque.addLast(randVal);
                 solutionDeque.addLast(randVal);
-                assertEquals("addLast(" + randVal + ")",
-                        solutionDeque.size(), studentDeque.size());
+            } else if (operationNumber == 1) {
+                Integer randVal = StdRandom.uniform(0, 100);
+                studentDeque.addFirst(randVal);
+                solutionDeque.addFirst(randVal);
             } else if (operationNumber == 2) {
-                // size
-                assertEquals("size()",
-                        solutionDeque.size(), studentDeque.size());
+                assertEquals("size()", solutionDeque.size(), studentDeque.size());
             } else if (operationNumber == 3) {
-                // removeFirst
-                if (!solutionDeque.isEmpty()) {
-                    Integer expected = solutionDeque.removeFirst();
-                    Integer actual = studentDeque.removeFirst();
-                    assertEquals("removeFirst()", expected, actual);
-                }
-            } else if (operationNumber == 4) {
-                // removeLast
-                if (!solutionDeque.isEmpty()) {
-                    Integer expected = solutionDeque.removeLast();
-                    Integer actual = studentDeque.removeLast();
-                    assertEquals("removeLast()", expected, actual);
-                }
-            } else if (operationNumber == 5) {
-                // get
-                if (!solutionDeque.isEmpty()) {
-                    int index = StdRandom.uniform(0, solutionDeque.size());
-                    Integer expected = solutionDeque.get(index);
-                    Integer actual = studentDeque.get(index);
-                    assertEquals("get(" + index + ")", expected, actual);
-                }
+                assertEquals("isEmpty()", solutionDeque.isEmpty(), studentDeque.isEmpty());
             }
         }
     }
 
     @Test
-    public void testAddFirstAndRemoveFirst() {
+    public void testRemoveFirst() {
         StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> solutionDeque = new ArrayDequeSolution<>();
 
-        for (int i = 0; i < 100; i++) {
-            studentDeque.addFirst(i);
-            solutionDeque.addFirst(i);
-        }
-
-        for (int i = 0; i < 100; i++) {
-            Integer expected = solutionDeque.removeFirst();
-            Integer actual = studentDeque.removeFirst();
-            assertEquals("removeFirst() after addFirst", expected, actual);
-        }
-    }
-
-    @Test
-    public void testAddLastAndRemoveLast() {
-        StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> solutionDeque = new ArrayDequeSolution<>();
-
-        for (int i = 0; i < 100; i++) {
+        // Add 50 elements
+        for (int i = 0; i < 50; i++) {
             studentDeque.addLast(i);
             solutionDeque.addLast(i);
         }
 
-        for (int i = 0; i < 100; i++) {
+        // Remove and compare
+        for (int i = 0; i < 50; i++) {
+            Integer expected = solutionDeque.removeFirst();
+            Integer actual = studentDeque.removeFirst();
+            assertEquals("removeFirst()", expected, actual);
+        }
+    }
+
+    @Test
+    public void testRemoveLast() {
+        StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solutionDeque = new ArrayDequeSolution<>();
+
+        // Add 50 elements
+        for (int i = 0; i < 50; i++) {
+            studentDeque.addLast(i);
+            solutionDeque.addLast(i);
+        }
+
+        // Remove and compare
+        for (int i = 0; i < 50; i++) {
             Integer expected = solutionDeque.removeLast();
             Integer actual = studentDeque.removeLast();
-            assertEquals("removeLast() after addLast", expected, actual);
+            assertEquals("removeLast()", expected, actual);
         }
     }
 
     @Test
-    public void testMixedOperations() {
+    public void testGet() {
         StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> solutionDeque = new ArrayDequeSolution<>();
 
-        // Mix of addFirst and addLast
+        // Add elements
         for (int i = 0; i < 50; i++) {
-            studentDeque.addFirst(i);
-            solutionDeque.addFirst(i);
-            studentDeque.addLast(i + 50);
-            solutionDeque.addLast(i + 50);
-        }
-
-        // Check size
-        assertEquals("size after mixed adds", solutionDeque.size(), studentDeque.size());
-
-        // Remove from both ends
-        for (int i = 0; i < 25; i++) {
-            Integer expectedFirst = solutionDeque.removeFirst();
-            Integer actualFirst = studentDeque.removeFirst();
-            assertEquals("removeFirst mixed", expectedFirst, actualFirst);
-
-            Integer expectedLast = solutionDeque.removeLast();
-            Integer actualLast = studentDeque.removeLast();
-            assertEquals("removeLast mixed", expectedLast, actualLast);
-        }
-    }
-
-    @Test
-    public void testGetAfterOperations() {
-        StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> solutionDeque = new ArrayDequeSolution<>();
-
-        for (int i = 0; i < 200; i++) {
             if (StdRandom.bernoulli()) {
                 studentDeque.addFirst(i);
                 solutionDeque.addFirst(i);
@@ -142,7 +91,7 @@ public class TestArrayDequeEC {
             }
         }
 
-        // Test get for all valid indices
+        // Test get
         for (int i = 0; i < solutionDeque.size(); i++) {
             Integer expected = solutionDeque.get(i);
             Integer actual = studentDeque.get(i);
@@ -151,12 +100,35 @@ public class TestArrayDequeEC {
     }
 
     @Test
-    public void testEmptyDeque() {
+    public void testMixedOperations() {
         StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solutionDeque = new ArrayDequeSolution<>();
 
-        assertTrue("isEmpty on new deque", studentDeque.isEmpty());
-        assertEquals("size on empty deque", 0, studentDeque.size());
-        assertNull("removeFirst on empty", studentDeque.removeFirst());
-        assertNull("removeLast on empty", studentDeque.removeLast());
+        for (int i = 0; i < 200; i++) {
+            int operationNumber = StdRandom.uniform(0, 5);
+
+            if (operationNumber == 0) {
+                Integer randVal = StdRandom.uniform(0, 100);
+                studentDeque.addFirst(randVal);
+                solutionDeque.addFirst(randVal);
+            } else if (operationNumber == 1) {
+                Integer randVal = StdRandom.uniform(0, 100);
+                studentDeque.addLast(randVal);
+                solutionDeque.addLast(randVal);
+            } else if (operationNumber == 2 && !solutionDeque.isEmpty()) {
+                Integer expected = solutionDeque.removeFirst();
+                Integer actual = studentDeque.removeFirst();
+                assertEquals("removeFirst()", expected, actual);
+            } else if (operationNumber == 3 && !solutionDeque.isEmpty()) {
+                Integer expected = solutionDeque.removeLast();
+                Integer actual = studentDeque.removeLast();
+                assertEquals("removeLast()", expected, actual);
+            } else if (operationNumber == 4 && !solutionDeque.isEmpty()) {
+                int index = StdRandom.uniform(0, solutionDeque.size());
+                Integer expected = solutionDeque.get(index);
+                Integer actual = studentDeque.get(index);
+                assertEquals("get(" + index + ")", expected, actual);
+            }
+        }
     }
 }
